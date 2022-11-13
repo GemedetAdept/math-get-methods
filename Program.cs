f3f36c877e8aa69f25de9117b599bdeb5a5c9475
Vivian Zitek <vivian.zitek@gmail.com>
2022-11-12 17:17:34 -0700

    Testing different concepts using MathGet methods.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿var pointA = new double[] {0.0, 0.0};
-var pointB = new double[] {1.0, 2.0};
+﻿double[] pointA = new double[2];
+double[] pointB = new double[2];
 
-Console.WriteLine(MathGet.LineLength2D(pointA, pointB));
+double[] inputPoint(double[] point, string pointName) {
+
+	Console.WriteLine("Input coordinates of first point; '(x, y)'");
+	Console.Write("> ");
+	string userInput = Console.ReadLine();
+	string[] splitInput = inputX.Replace("(", "").Replace(")", "").Split(", ");
+
+	point = splitInputX.Select(a => Convert.ToDouble(a)).ToArray();
+}
+
+Console.WriteLine(pointA[0] + ", " + pointA[1]);
